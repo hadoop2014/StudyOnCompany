e2789d00054549bab60c73756e2519ac2a9aaf19
hadoop2014 <wwyandotte@hotmial.com>
2020-06-01 23:57:26 +0800

    简化了interpretAccounting的语法

--- interpreter/interpretAccounting.py
+++ interpreter/interpretAccounting.py
@@ -25,20 +25,8 @@ class interpretAccounting(interpretBase):
             local_name['t_'+token] = self.dictTokens[token]
         print(str({key:value for key,value in local_name.items() if key.split('_')[-1] in tokens}).replace("',","'\n"))
 
-        #def t_PERCENTAGE(t):
-        #    '\\d+[.\\d*]*%'
-        #    t.value = int(t.value[:-1])*0.01
-        #    print(t.value)
-        #    return t
-
-        #def t_NUMBER(t):
-        #    r'\d+'
-        #    t.value = int(t.value)
-        #    return t
-
-
         #t_ignore = " \t\n"
-        t_ignore = self.ignores + local_name['t_PUNCTUATION']
+        t_ignore = self.ignores
 
         def t_newline(t):
             r'\n+'
@@ -64,126 +52,86 @@ class interpretAccounting(interpretBase):
         # dictionary of names
         names = {}
 
-        #def p_statement_assign(p):
-        #    'statement : NAME "=" expression'
-        #    names[p[1]] = p[3]
         def p_statement_statement(p):
             '''statement : statement statement'''
-            print(p[1],p[2])
+            #print(p[1],p[2])
+            p[0] = p[1]
 
-        def p_statement_discardshiftl(p):
-            '''statement : DISCARD statement'''
-            print(p[1],p[2])
 
         def p_statement_expr(p):
             '''statement : expression'''
-            print(p[1])
+            p[0] = p[1]
 
         def p_expression_search(p):
-            '''expression : TABLE expression'''
+            '''expression : TABLE TIME UNIT expression'''
             print("search",p[1],p[2])
-            names[p[1]] = {'time':p[2],'unit':names['unit'],'currency':names['currency']}
+            names['unit'] = p[3].split(':')[-1].split('：')[-1]
+            names[p[1]] = {'table':p[1],'time':p[2],'unit':names['unit'],'currency':names['currency']}
             print(names[p[1]])
 
-        #def p_expression_binop(p):
-        #    '''expression : expression '+' expression
-        #                  | expression '-' expression
-        #                  | expression '*' expression
-        #                  | expression '/' expression'''
-        #    if p[2] == '+':
-        #        p[0] = p[1] + p[3]
-        #    elif p[2] == '-':
-        #        p[0] = p[1] - p[3]
-        #    elif p[2] == '*':
-        #        p[0] = p[1] * p[3]
-        #    elif p[2] == '/':
-        #        p[0] = p[1] / p[3]
-        #def p_expression_discardshiftl(p):
-        #    'expression : DISCARD expression'
-        #    p[0] = p[2]  #跳过DISCARD
-        #    print(p[0])
+        def p_expression_searchealy(p):
+            '''expression : TABLE UNIT expression'''
+            print("search",p[1],p[2])
+            names['unit'] = p[2].split(':')[-1].split('：')[-1]
+            names[p[1]] = {'table':p[1],'unit':names['unit'],'currency':names['currency']}
+            print(names[p[1]])
 
-        #def p_expression_discardshiftr(p):
-        #    'expression : expression DISCARD'
-        #    #p[0] = p[2]  #跳过DISCARD
-        #    print(p[0])
-
-        def p_expression_discard(p):
-            '''expression : DISCARD'''
-            #p[0] = p[2]  #跳过DISCARD
-            print(p[0])
-
-        def p_expression_unitshiftl(p):
-            'expression : UNIT expression'
-            #p[0] = p[2]  #跳过DISCARD
-            names['unit'] = p[1]
+        def p_expression_searchnone(p):
+            '''expression : TABLE expression'''
             p[0] = p[1]
-            print(p[1])
-
-        def p_expression_timeshiftl(p):
-            'expression : TIME expression'
-            p[0] = p[1]
-            print(p[0])
-
-        def p_expression_header(p):
-            'expression : HEADER'
-            p[0] = p[1]
-            print(p[0])
-
-        def p_expression_percentage(p):
-            'expression : PERCENTAGE'
-            p[0] = p[1]
-            print(p[0])
-
-        def p_expression_value(p):
-            'expression : VALUE'
-            p[0] = float(p[1].replace(',',''))
-            print(p[0],p[1])
-
-        #def p_expression_uminus(p):
-        #    "expression : '-' expression %prec UMINUS"
-        #    p[0] = -p[2]
-
-        def p_expression_valueuminus(p):
-            "expression : '-' VALUE %prec UMINUS"
-            p[0] = -float(p[2].replace(',',''))
-
-        def p_expression_numberuminus(p):
-            "expression : '-' NUMBER %prec UMINUS"
-            p[0] = -int(p[2])
 
         def p_expression_group(p):
             "expression : '(' expression ')'"
             p[0] = p[2]
 
-
-        def p_expression_number(p):
-            "expression : NUMBER"
-            p[0] = int(p[1])
-            print(p[0])
-
-        #def p_expression_name(p):
-        #    "expression : NAME"
-        #    try:
-        #        p[0] = names[p[1]]
-        #    except LookupError:
-        #        print("Undefined name '%s'" % p[1])
-        #        p[0] = 0
-
-        #def p_expression_unit(p):
-        #    'expression : UNIT expression'
-        #    names['unit'] = p[1]
-        #    p[0] = p[1]
-
         def p_expression_currency(p):
-            'expression : CURRENCY'
-            names['currency'] = p[1]
+            '''expression : CURRENCY'''
+            names['currency'] = p[1].split(':')[-1].split('：')[-1]
             p[0] = p[1]
             print(p[0])
 
-        def p_expression_punctuation(p):
-            '''expression : PUNCTUATION'''
-            print(p[1])
+        def p_expression_nothing(p):
+            '''expression : nothing'''
+            p[0] = p[1]
+
+        def p_nothing(p):
+            '''nothing : PUNCTUATION
+                       | DISCARD
+                       | WEBSITE
+                       | EMAIL
+                       | COMPANY
+                       | NAME
+                       | TIME
+                       | HEADER
+                       | UNIT'''
+            print('nothing ',p[1])
+
+        def p_expression_term(p):
+            '''expression : term'''
+            p[0] = p[1]
+
+        def p_term_percentage(p):
+            '''term : PERCENTAGE'''
+            p[0] = float(p[1][:-1]) * 0.01
+            print('percentage',p[0])
+
+        def p_term_value(p):
+            '''term : VALUE'''
+            p[0] = float(p[1].replace(',',''))
+            print('value',p[0],p[1])
+
+        def p_term_number(p):
+            '''term : NUMBER'''
+            p[0] = int(p[1])
+            print('number',p[0])
+
+        def p_term_uminus(p):
+            '''term : '-' term %prec UMINUS'''
+            p[0] = -p[2]
+
+        def p_term_group(p):
+            '''term : '(' term ')' '''
+            p[0] = -p[2]  #财务报表中()表示负值
 
         def p_error(p):
             if p:
@@ -196,9 +144,10 @@ class interpretAccounting(interpretBase):
         self.parser = yacc.yacc(outputdir=self.working_directory)
 
     def doWork(self,docParser):
-        #for data in docParser:
-        #    self.parser.parse(docParser._get_text(data).replace(' ',''))
+        for data in docParser:
+            self.parser.parse(docParser._get_text(data).replace(' ',''))
 
+        '''
         item = 76
         data = docParser._get_item(item)
         text = docParser._get_text(data)
@@ -207,7 +156,7 @@ class interpretAccounting(interpretBase):
         for token in self.lexer:
             print(token)
         self.parser.parse(text,lexer=self.lexer,debug=True)
-
+        '''
         docParser._close()
 
 def create_object(gConfig):
